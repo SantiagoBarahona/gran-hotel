c4973b4fa4bd6033a15aec042fd9cc2a21dd1127
santiago <llsanthiago@gmail.com>
2025-02-05 09:13:01 -0500

    Slider images data on his own file

---
+++ src/data/slider.data.js
@@ -0,0 +1,11 @@
+export const homeSliderImages = [
+    "/home/gran-hotel.jpg",
+    "/home/restaurante.avif",
+    "/home/balcon.avif",
+    "/home/habitacion.avif",
+    "/home/recepcion.avif",
+    "/home/restaurante-2.avif",
+    "/home/recepcion-2.avif",
+    "/home/cenital.avif",
+];
+
