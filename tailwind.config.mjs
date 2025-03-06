bf5e4b9d48d4b2dda7a75cabc2ef29be07623112
Santiago Zapata Barahona <llsanthiago@gmail.com>
2025-03-05 23:47:38 -0500

    style: added more spacing between sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,24 @@
-import typography from "@tailwindcss/typography"
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'javascript': '#f7df1e',
-				'javascript-dark': '#998c06',
-				'light-gray': '#7F7F7F',
-				'red-granhotel': 'rgb(170, 0, 0)'
-			},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        javascript: "#f7df1e",
+        "javascript-dark": "#998c06",
+        "light-gray": "#7F7F7F",
+        "red-granhotel": "rgb(170, 0, 0)",
+      },
 
-			padding:{
-				'section': '4rem',
-				'section-mobile': '2rem',
-				'section-vertical': '2rem',
-				'section-vertical-mobile': '0.5rem',
-			}
-		},
-	},
-	plugins: [typography()],
-}
+      padding: {
+        section: "4rem",
+        "section-mobile": "2rem",
+        "section-vertical": "4rem",
+        "section-vertical-mobile": "0.5rem",
+      },
+    },
+  },
+  plugins: [typography()],
+};
